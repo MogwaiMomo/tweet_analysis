cbf0eb495e22549599ae3201e59973c9e7f15b13
Momoko Price <momokoprice@gmail.com>
2020-11-10 11:05:16 -0800

    created anger and joy wordclouds using sentiment analysis

--- main.R
+++ main.R
@@ -79,6 +79,7 @@ if (get.tweets == "y") {
   file <- readline(prompt = "Please type the filename of the file you want to open: ")
   # Load data back in from large CSV file
   raw_tweets <- fread(file)
+  query_file_name <- str_split(file, fixed("."), n=2)[[1]][1]
   print("Done! Have fun text-mining :)")
 } else {
   print("sorry, didn't catch that.")
@@ -151,7 +152,6 @@ top5_hashtags <- raw_tweets %>%
   
 # word cloud visualization
 library(wordcloud)
-
 words <- raw_tweets %>%
   mutate(text = str_remove_all(text, "&amp;|&lt;|&gt;"),
          text = str_remove_all(text, "\\s?(f|ht)(tp)(s?)(://)([^\\.]*)[\\.|/](\\S*)"),
@@ -165,25 +165,76 @@ words <- raw_tweets %>%
   filter(word != query) %>%
   count(word, sort = TRUE)
 
-wordcloud(words$word, words$n, random.order = FALSE, max.words = 100, color = alpha("blue", seq(0.4,1, 0.05)))
-
-cloud <- words %>% 
-  with(wordcloud(word, n, random.order = FALSE, max.words = 100, color = alpha("blue", seq(0.4,1, 0.05))))
-
 
 # create color gradient for word cloud
 cloud_filename <- paste0(query_file_name, "_wordcloud.png")
 
-png(filename="testplot.png", 
+png(filename=cloud_filename, 
     width=500, 
     height=500,
     units="px")
 wordcloud(words$word, words$n, random.order = FALSE, max.words = 100, color = alpha("blue", seq(0.4,1, 0.05)))
 dev.off()
 
-plot(sin, -pi, 2*pi)
-
-
 
 # NEXT UP: Work through tidy text mining text book: https://www.tidytextmining.com/sentiment.html
-  
+
+
+# Step 1: Create a sentiment analysis
+
+tidy_tweets <- raw_tweets %>%
+  mutate(text = str_remove_all(text, "&amp;|&lt;|&gt;"),
+         text = str_remove_all(text, "\\s?(f|ht)(tp)(s?)(://)([^\\.]*)[\\.|/](\\S*)"),
+         text = str_remove_all(text, "[^\x01-\x7F]")) %>% 
+  unnest_tokens(word, text, token = "tweets") %>%
+  filter(!word %in% stop_words$word,
+         !word %in% str_remove_all(stop_words$word, "'"),
+         str_detect(word, "[a-z]"),
+         !str_detect(word, "^#"),         
+         !str_detect(word, "@\\S+")) %>%
+  filter(word != query) %>%
+  filter(word != "vote")
+
+nrc_joy <- get_sentiments("nrc") %>%
+  filter(sentiment == "joy")
+
+joy_words <- tidy_tweets %>%
+  inner_join(nrc_joy) %>%
+  count(word, sort = T)
+
+# joy wordcloud from election2020
+
+png(filename="election2020_joy_words.png", 
+    width=500, 
+    height=500,
+    units="px",
+    res=140)
+wordcloud(joy_words$word, 
+          joy_words$n, 
+          random.order = FALSE, 
+          max.words = 200, 
+          color = alpha("purple", seq(0.4,1, 0.05))
+          )
+dev.off()
+
+# anger wordcloud from election2020
+
+nrc_anger <- get_sentiments("nrc") %>%
+  filter(sentiment == "anger")
+
+anger_words <- tidy_tweets %>%
+  inner_join(nrc_anger) %>%
+  count(word, sort = T)
+
+png(filename="election2020_anger_words.png", 
+    width=500, 
+    height=500,
+    units="px",
+    res=140)
+wordcloud(anger_words$word, 
+          anger_words$n, 
+          random.order = FALSE, 
+          max.words = 200, 
+          color = alpha("red", seq(0.4,1, 0.05))
+)
+dev.off()
