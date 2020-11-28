9d444952becd690154064e5a519118afcdc0fa60
Momoko Price <momokoprice@gmail.com>
2020-11-28 12:08:28 -0800

    started adjusting code to accommodate AFINN-based analysis

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -3,13 +3,16 @@ get_sentiments_words <- function(text_df, omit_words, lexicon, emotion=NULL){
   # create tidy tokens for analysis
   tidy_tweets <- create_tidy_tokens(text_df, "text", omit_words)
   
-  # joy wordcloud from election2020
-  sent_dataset <- get_sentiments(lexicon) %>%
-    filter(sentiment == emotion)
-  
-  sentiment_words <- tidy_tweets %>%
-    inner_join(sent_dataset) %>%
-    count(word, sort = T)
+  if (lexicon == "afinn") {
+    print("don't have afinn-based analysis ready yet.")
+  } else {
+    sent_dataset <- get_sentiments(lexicon) %>%
+      filter(sentiment == emotion)
+    
+    sentiment_words <- tidy_tweets %>%
+      inner_join(sent_dataset) %>%
+      count(word, sort = T)
+  }
   
   return(sentiment_words)
 }  
