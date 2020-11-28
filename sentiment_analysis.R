bef8b56fac4a527cc877ef3bddcbb3435f4c6081
Momoko Price <momokoprice@gmail.com>
2020-11-28 11:58:49 -0800

    refactor sent analysis code into separate file

---
+++ sentiment_analysis.R
@@ -0,0 +1,16 @@
+# sentiment analysis
+get_sentiments_words <- function(text_df, omit_words, lexicon, emotion){
+  # create tidy tokens for analysis
+  tidy_tweets <- create_tidy_tokens(text_df, "text", omit_words)
+  
+  # joy wordcloud from election2020
+  sent_dataset <- get_sentiments(lexicon) %>%
+    filter(sentiment == emotion)
+  
+  sentiment_words <- tidy_tweets %>%
+    inner_join(sent_dataset) %>%
+    count(word, sort = T)
+  
+  return(sentiment_words)
+}  
+
