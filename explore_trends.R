7d9cdf8cc35391d38713eb5f09467ba37b75687b
Momoko Price <momokoprice@gmail.com>
2020-12-13 15:35:51 -0800

    refactor exploratory analysis code

---
+++ explore_trends.R
@@ -0,0 +1,142 @@
+
+explore_df <- function(df) {
+  
+  # EXPLORE DATA #
+  ################
+  
+  # 1: check obj structure, change int to factors
+  
+  df <- df %>%
+    mutate_if(is.integer, as.factor) %>%
+    mutate_if(is.character, as.factor)
+  str(df) # check if organization makes sense
+  
+  # Adjust as needed: 
+  
+  # # horsepower -> change to numeric
+  # df$horsepower <- as.numeric(df$horsepower)
+  # 
+  # # weight -> change to numeric
+  # df$weight <- as.numeric(df$weight)
+  # 
+  # # name -> change to character
+  # df$name <- as.character(df$name)
+  # 
+  # # check again
+  # str(df)
+  
+  # 2: Get summary stats
+  
+  # # use summary to display stats of quants
+  # 
+  # quants <-df %>% select_if(is.numeric)
+  # quals <- df %>% select_if(is.factor)
+  # labels <- df %>% select_if(is.character)
+  # 
+  # 
+  # # function to summarize key stats - NEED TO CLEAN THIS UP
+  # get_stats <- function(quants){
+  #   
+  #   # get min, max, mean, etc.
+  #   stats.table <- as.df.frame.matrix(summary(quants))
+  #   names(stats.table) <- trimws(names(stats.table))
+  #   # ugly hack to calculate sd & append to the summary table
+  #   # get sd as vector
+  #   stats.sd <- df.frame("sd" = sapply(quants, sd)) %>%
+  #     round(digit = 4)
+  #   # reformat
+  #   formatted.stats.sd <- stats.sd %>%
+  #     # preserve var names
+  #     as.df.table(keep.rownames = T) %>%
+  #     # add formatting to match summary table
+  #     mutate(sd = paste("Sd : ", sd)) %>%
+  #     # transpose to wide
+  #     t %>% df.table(keep.rownames = T) %>%
+  #     # drop redundant first col
+  #     dplyr::select(-1)
+  #   # assign first row to table names
+  #   names(formatted.stats.sd) <- unlist(formatted.stats.sd[1,])
+  #   # get rid of redundant first row
+  #   final <- formatted.stats.sd[2,]
+  #   
+  #   # STILL TO DO: append sd to stats table
+  #   
+  #   stats.table.tmp <- rbind(stats.table[1:4,], final)
+  #   stats.table <- rbind(stats.table.tmp, stats.table[5:6,])
+  #   
+  #   return(stats.table)
+  # }
+  # 
+  # stats.summary <-  get_stats(quants) 
+  # 
+  # # 3. Graph & explore variables 
+  # 
+  # ## Plot all factor-based graphs
+  # 
+  # # set grid of plots 1rx3c
+  # par(mfrow=c(1,3))
+  # attach(df)
+  # boxplot(mpg ~ cylinders, xlab = "cyl")
+  # boxplot(mpg ~ year, xlab = "year")
+  # boxplot(mpg ~ origin, xlab = "origin")
+  # 
+  # 
+  # ## plot all single-var & two-var quant graphs
+  # 
+  # library(PerformanceAnalytics)
+  # png(filename="plot.png", width = 1480, height = 1480)
+  # plot2 <- chart.Correlation(quants,hist=T) 
+  # dev.off()
+  
+  
+}
+
+# 
+# 
+# ### Interesting questions for analysis
+# 
+# # How are sentiment scores in this sample distributed?  
+# 
+# ## 1. Significance test for normality for sent scores (H0 = is normal)
+# 
+# ### First, get a smaller representation for shapiro test (5000 max)
+# shap_sample <- sample_n(sa_tweets, 500)
+# 
+# ### Get significance
+# shapiro.test(shap_sample$ave_sentiment)$p.value # result: not normal. 
+# 
+# ## 2a. Visualize as histogram
+# ggplot(sa_tweets, aes(ave_sentiment)) +
+#   geom_histogram(fill="black", colour="black", alpha = 0.25, binwidth=0.05) + 
+#   geom_density(aes(y=0.05*..count..), colour="black", adjust=4) +
+#   geom_vline(aes(xintercept = mean(ave_sentiment)), colour="red") +
+#   theme(legend.position="none")
+# 
+# 
+# ## 2b. Visualize as a boxplot (verified vs. unverified)
+# ggplot(sa_tweets, aes(x=verified, y=ave_sentiment)) +
+#   geom_boxplot(fill="black", colour="black", alpha = 0.25) + 
+#   theme(legend.position="none")
+# 
+# # How does average sentiment change over time?
+# 
+# # Step 1. Categorize by day
+
+
+
+
+#### NLP STUFF #######
+
+# Get Top 5 summary tables 
+#top5_summaries(tweets, query_file_name)
+
+# Generate nrc sentiment analysis
+# omit_words <- c(query, "vote") # adjust as needed
+# nrc_words <- get_sentiments_words(tweets, omit_words, "nrc", "joy")
+# create_wordcloud(cloud_words, "output/nrc_cloud.png")
+# 
+# # Generate sentiment analysis with AFINN
+# afinn_words <- get_sentiments_words(tweets, omit_words, "afinn", "negative")
+# create_wordcloud(afinn_words, "output/afinn_cloud.png")
+
+
--- main.R
+++ main.R
@@ -20,7 +20,7 @@ source("top_5_analysis.R")
 source("create_wordcloud.R")
 source("tidy_tokens.R")
 source("sentiment_analysis.R")
-
+source("explore_trends.R")
 
 # define necessary string vars
 output_path <- "data/"
@@ -29,72 +29,45 @@ end_date <- "2020-11-18" # how far back you want to go in time
 date.string <- as.character(Sys.Date())
 query_file_name <- paste0(output_path, query, "_", end_date, "_to_", date.string, "_tweets.csv")
 
-# load a specific file for work
-file <- "data/election2020_2020-11-18_to_2020-11-24_tweets.csv"
-tweets <- fread(file, na.strings = c("",NA))
-query <- str_split(file, "_")[[1]][1] %>%
-  str_replace("data/", "")
+
+
+start <- readline(prompt = "Load default file (F) or pull tweets (T)? ")
+
+if (start == "F") {
+  # load a specific file for work
+  file <- "data/election2020_2020-11-18_to_2020-11-24_tweets.csv"
+  tweets <- fread(file, na.strings = c("",NA))
+  query <- str_split(file, "_")[[1]][1] %>%
+    str_replace("data/", "")
+  
+} else if (start == "T") {
+  # authenticate twitter
+  authenticate_twitter()
+  tweets <- pull_max_tweets(query, end_date)
+  save_as_csv(tweets, file_name=query_file_name)
+  # load back in for clean processing
+  tweets <- fread(query_file_name, na.strings = c("",NA))
+  
+} else {
+  
+  print("Didn't catch that, please try again.")
+  
+}
+
 
 # take only cols of interest for text-mining analysis
 tweets %>% 
   select(c(1:5,13,14,17,78,83,84)) -> tweets
 
-# get sentiment analysis
+# get sentiment scores per document
 sa_tweets <- document_level_sa(tweets)
 
 
-### Interesting questions for analysis
-
-# How are sentiment scores in this sample distributed?  
-
-## 1. Significance test for normality for sent scores (H0 = is normal)
-
-### First, get a smaller representation for shapiro test (5000 max)
-shap_sample <- sample_n(sa_tweets, 500)
-
-### Get significance
-shapiro.test(shap_sample$ave_sentiment)$p.value # result: not normal. 
-
-## 2a. Visualize as histogram
-ggplot(sa_tweets, aes(ave_sentiment)) +
-  geom_histogram(fill="black", colour="black", alpha = 0.25, binwidth=0.05) + 
-  geom_density(aes(y=0.05*..count..), colour="black", adjust=4) +
-  geom_vline(aes(xintercept = mean(ave_sentiment)), colour="red") +
-  theme(legend.position="none")
-  
-
-## 2b. Visualize as a boxplot (verified vs. unverified)
-ggplot(sa_tweets, aes(x=verified, y=ave_sentiment)) +
-  geom_boxplot(fill="black", colour="black", alpha = 0.25) + 
-  theme(legend.position="none")
-
-# How does average sentiment change over time?
-
-# Step 1. Categorize by day
+# run exploratory analysis
+explore_df(sa_tweets)
 
 
 
-# pull fresh tweets and save to file
-
-# # authenticate twitter
-# authenticate_twitter()
-# tweets <- pull_max_tweets(query, end_date)
-# save_as_csv(tweets, file_name=query_file_name)
-# # load back in for clean processing
-# tweets <- fread(query_file_name, na.strings = c("",NA))
-
-
-# Get Top 5 summary tables 
-#top5_summaries(tweets, query_file_name)
-
-# Generate nrc sentiment analysis
-# omit_words <- c(query, "vote") # adjust as needed
-# nrc_words <- get_sentiments_words(tweets, omit_words, "nrc", "joy")
-# create_wordcloud(cloud_words, "output/nrc_cloud.png")
-# 
-# # Generate sentiment analysis with AFINN
-# afinn_words <- get_sentiments_words(tweets, omit_words, "afinn", "negative")
-# create_wordcloud(afinn_words, "output/afinn_cloud.png")
 
 
 
